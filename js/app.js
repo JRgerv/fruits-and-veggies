78007991ed61d2d403432e90b4ad11d4a089afcf
JRgerv <jack.gervais@gmail.com>
2017-03-15 09:11:51 -0700

    middle column concat

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 /* setup your angular app here */
+var app = angular.module('fnv',[]);
+
+app.controller('sort', ['$scope', function($scope){
+  $scope.fruit = fruits;
+  $scope.veggie = vegetables;
+  $scope.unsorted = unsorted;
+    
+
+
+
+
+}]);
+
+
 
 // debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
 console.log('Fruit count', fruits.length);
 console.log('Veggie count', vegetables.length);
+console.log('unsorted count', unsorted.length);
